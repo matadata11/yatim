340f5f00540d18552d4149dbfbb5ff5761b45439
matadata11 <matadata.dev2021@gmail.com>
2022-12-05 13:30:06 +0700

    login

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,7 +67,7 @@
             </div>
             <div class="col-lg-7 d-none d-lg-block">
                 <div id="auth-right">
-                
+
                 </div>
             </div>
         </div>
